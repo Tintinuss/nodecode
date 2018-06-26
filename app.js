a5e3fe3eaae347286ce6a288cfb77b0e950f75c2
Tintinuss <31693335+Tintinuss@users.noreply.github.com>
2018-06-26 11:29:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ var app = express();
 
 app.use(express.static('public'));
 console.log('Exact name: ' + process.env.ORA_INSTANCE_NAME);
-//runGetRequest();
-runDatabaseQuery();
+runGetRequest();
+//runDatabaseQuery();
 
 // Does a GET request to ip.jsontest.com
 function runGetRequest() {
     
     //sample URL.
-    var url = "http://140.86.15.104:3000/fighters/45/0/green/Tintinuss";
+    var url = "http://140.86.15.104:3000/reactorCore/320/650/green/Tintinuss";
     request(url, function(error, response, body) {
         if(!error) {
             console.log(body);
