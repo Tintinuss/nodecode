4a55a177c11ea6b1fc4091c6ac68971c1a281590
Tintinuss <31693335+Tintinuss@users.noreply.github.com>
2018-06-26 11:19:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var app = express();
 
 app.use(express.static('public'));
 console.log('Exact name: ' + process.env.ORA_INSTANCE_NAME);
-runGetRequest();runDatabaseQuery();
+//runGetRequest();
+runDatabaseQuery();
 
 // Does a GET request to ip.jsontest.com
 function runGetRequest() {
